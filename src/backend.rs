1afaa3cc0bde7da9a5edb1850fa2f1d49562f80b
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-11-05 16:23:48 +0100

    Implement basics.

--- src/backend.rs
+++ src/backend.rs
@@ -3,6 +3,8 @@
 use dynasmrt::x64::Assembler;
 use dynasmrt::DynasmApi;
 
+const WORD_SIZE: u32 = 8;
+
 type GPR = u8;
 
 struct GPRs {
@@ -15,22 +17,22 @@ impl GPRs {
     }
 }
 
-static RAX: u8 = 0;
-static RCX: u8 = 1;
-static RDX: u8 = 2;
-static RBX: u8 = 3;
-static RSP: u8 = 4;
-static RBP: u8 = 5;
-static RSI: u8 = 6;
-static RDI: u8 = 7;
-static R8: u8 = 8;
-static R9: u8 = 9;
-static R10: u8 = 10;
-static R11: u8 = 11;
-static R12: u8 = 12;
-static R13: u8 = 13;
-static R14: u8 = 14;
-static R15: u8 = 15;
+const RAX: u8 = 0;
+const RCX: u8 = 1;
+const RDX: u8 = 2;
+const RBX: u8 = 3;
+const RSP: u8 = 4;
+const RBP: u8 = 5;
+const RSI: u8 = 6;
+const RDI: u8 = 7;
+const R8: u8 = 8;
+const R9: u8 = 9;
+const R10: u8 = 10;
+const R11: u8 = 11;
+const R12: u8 = 12;
+const R13: u8 = 13;
+const R14: u8 = 14;
+const R15: u8 = 15;
 
 impl GPRs {
     fn take(&mut self) -> GPR {
@@ -75,31 +77,146 @@ impl Registers {
     }
 }
 
-fn push_i32(ops: &mut Assembler, regs: &mut Registers, gpr: GPR) {
+/// Describes location of a argument.
+enum ArgLocation {
+    /// Argument is passed via some register.
+    Reg(GPR),
+    /// Value is passed thru the stack.
+    Stack(i32),
+}
+
+/// Get a location for an argument at the given position.
+fn abi_loc_for_arg(pos: u32) -> ArgLocation {
+    // TODO: This assumes only system-v calling convention.
+    const ARGS_IN_GPRS: &'static [GPR] = &[
+        RDI,
+        RSI,
+        RDX,
+        RCX,
+    ];
+
+    if pos < ARGS_IN_GPRS.len() as u32 {
+        ArgLocation::Reg(ARGS_IN_GPRS[pos as usize])
+    } else {
+        let stack_pos = pos - ARGS_IN_GPRS.len() as u32;
+        ArgLocation::Stack((stack_pos * WORD_SIZE) as i32)
+    }
+}
+
+pub struct Context {
+    regs: Registers,
+    /// Offset from starting value of SP. Updated on every push or pop
+    /// on the value stack.
+    sp_depth: usize,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Context {
+            regs: Registers::new(),
+            sp_depth: 0,
+        }
+    }
+}
+
+fn push_i32(ctx: &mut Context, ops: &mut Assembler, gpr: GPR) {
     // For now, do an actual push (and pop below). In the future, we could
     // do on-the-fly register allocation here.
+    ctx.sp_depth += 1;
     dynasm!(ops
         ; push Rq(gpr)
     );
-    regs.release_scratch_gpr(gpr);
+    ctx.regs.release_scratch_gpr(gpr);
 }
 
-fn pop_i32(ops: &mut Assembler, regs: &mut Registers) -> GPR {
-    let gpr = regs.take_scratch_gpr();
+fn pop_i32(ctx: &mut Context, ops: &mut Assembler) -> GPR {
+    ctx.sp_depth -= 1;
+    let gpr = ctx.regs.take_scratch_gpr();
     dynasm!(ops
         ; pop Rq(gpr)
     );
     gpr
 }
 
-pub fn add_i32(ops: &mut Assembler, regs: &mut Registers) {
-    let op0 = pop_i32(ops, regs);
-    let op1 = pop_i32(ops, regs);
+pub fn add_i32(ctx: &mut Context, ops: &mut Assembler) {
+    let op0 = pop_i32(ctx, ops);
+    let op1 = pop_i32(ctx, ops);
     dynasm!(ops
         ; add Rq(op0), Rq(op1)
     );
-    push_i32(ops, regs, op0);
-    regs.release_scratch_gpr(op1);
+    push_i32(ctx, ops, op0);
+    ctx.regs.release_scratch_gpr(op1);
+}
+
+fn sp_relative_offset(ctx: &mut Context, slot_idx: u32) -> i32 {
+    ((ctx.sp_depth as i32) + slot_idx as i32) * WORD_SIZE as i32
+}
+
+pub fn load_i32(ctx: &mut Context, ops: &mut Assembler, local_idx: u32) {
+    let gpr = ctx.regs.take_scratch_gpr();
+    let offset = sp_relative_offset(ctx, local_idx);
+    dynasm!(ops
+        ; mov Rq(gpr), [rsp + offset]
+    );
+    push_i32(ctx, ops, gpr);
+}
+
+pub fn store_i32(ctx: &mut Context, ops: &mut Assembler, local_idx: u32) {
+    let gpr = pop_i32(ctx, ops);
+    let offset = sp_relative_offset(ctx, local_idx);
+    dynasm!(ops
+        ; mov [rsp + offset], Rq(gpr)
+    );
+    ctx.regs.release_scratch_gpr(gpr);
+}
+
+pub fn prepare_return_value(ctx: &mut Context, ops: &mut Assembler) {
+    let ret_gpr = pop_i32(ctx, ops);
+    if ret_gpr != RAX {
+        dynasm!(ops
+            ; mov Rq(RAX), Rq(ret_gpr)
+        );
+        ctx.regs.release_scratch_gpr(ret_gpr);
+    }
+}
+
+pub fn copy_incoming_arg(ctx: &mut Context, ops: &mut Assembler, arg_pos: u32) {
+    let loc = abi_loc_for_arg(arg_pos);
+
+    // First, ensure the argument is in a register.
+    let reg = match loc {
+        ArgLocation::Reg(reg) => reg,
+        ArgLocation::Stack(offset) => {
+            // RAX is always scratch?
+            dynasm!(ops
+                ; mov Rq(RAX), [rsp + offset]
+            );
+            RAX
+        }
+    };
+
+    // And then move a value from a register into local variable area on the stack.
+    let offset = sp_relative_offset(ctx, arg_pos);
+    dynasm!(ops
+        ; mov [rsp + offset], Rq(reg) 
+    );
+}
+
+pub fn prologue(_ctx: &mut Context, ops: &mut Assembler, stack_slots: u32) {
+    let framesize: i32 = stack_slots as i32 * WORD_SIZE as i32;
+    dynasm!(ops
+        ; push rbp
+        ; mov rbp, rsp
+        ; sub rsp, framesize
+    );
+}
+
+pub fn epilogue(_ctx: &mut Context, ops: &mut Assembler) {
+    dynasm!(ops
+        ; mov rsp, rbp
+        ; pop rbp
+        ; ret
+    );
 }
 
 pub fn unsupported_opcode(ops: &mut Assembler) {
--- src/function_body.rs
+++ src/function_body.rs
@@ -5,23 +5,46 @@ use wasmparser::{FunctionBody, Operator};
 
 pub fn translate(body: &FunctionBody) -> Result<(), Error> {
     let locals = body.get_locals_reader()?;
+
+    // Assume signature is (i32, i32) -> i32 for now.
+    // TODO: Use a real signature
+    const ARG_COUNT: u32 = 2;
+
+    let mut framesize = ARG_COUNT;
     for local in locals {
-        local?; // TODO
+        let (count, _ty) = local?;
+        framesize += count;
     }
 
     let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+    let mut ctx = Context::new();
     let operators = body.get_operators_reader()?;
-    let mut regs = Registers::new();
+
+    prologue(&mut ctx, &mut ops, framesize);
+
+    for arg_pos in 0..ARG_COUNT {
+        copy_incoming_arg(&mut ctx, &mut ops, arg_pos);
+    }
+
     for op in operators {
         match op? {
             Operator::I32Add => {
-                add_i32(&mut ops, &mut regs);
+                add_i32(&mut ctx, &mut ops);
+            }
+            Operator::GetLocal { local_index } => {
+                load_i32(&mut ctx, &mut ops, local_index);
+            }
+            Operator::End => {
+                // TODO: This is super naive and makes a lot of unfounded assumptions 
+                // but will for the start.
+                prepare_return_value(&mut ctx, &mut ops);
             }
             _ => {
                 unsupported_opcode(&mut ops);
             }
         }
     }
+    epilogue(&mut ctx, &mut ops);
 
     let output = ops
         .finalize()
